9dc0e8e632d457c44e4a9a8b67ce867f1b220168
Pau Capellá Ballester <pau.capella@hotmail.es>
2025-02-17 21:55:46 +0100

    drag and drop with gravity and delete

--- potion-recall/Scripts/drop_area.gd
+++ potion-recall/Scripts/drop_area.gd
@@ -1,10 +1,1 @@
-extends StaticBody2D
-
-func _ready() -> void:
-	modulate = Color(Color.MEDIUM_PURPLE, 0.7)
-	
-func _process(delta: float) -> void:
-	if Global.is_dragging:
-		visible = true
-	else:
-		visible = false
+extends Area2D
