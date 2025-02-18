ea57531a4a162373086cef1c8dc518de6c5d0328
marc-ms <myphysics28@gmail.com>
2025-02-18 18:10:07 +0100

    cleaning random list folder

--- potion-recall/Scripts/control.gd
+++ potion-recall/Scripts/control.gd
@@ -3,23 +3,15 @@ extends Control
 var items = ["apple", "watermelon", "potato", "tomato", "cucumber", "carrot", "pear", "pinapple"]
 var levelNumber = 1
 
-var clock: ClockFace
-var time = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	for i in pick_n_random_values(items, levelNumber*2):
 		$ItemList.add_item(i)
-	
-#func _physics_process(delta: float) -> void:
-	#time += delta
-	#clock = ClockFace.new()
-	#clock.hand_rotation(time)
 
 func pick_n_random_values(array: Array, n: int) -> Array:
 	var array_copy: Array = array.duplicate()
 	array_copy.shuffle()
 	return array_copy.slice(0, n)
-
 
 func _on_button_pressed() -> void:
 	$ItemList.clear()
