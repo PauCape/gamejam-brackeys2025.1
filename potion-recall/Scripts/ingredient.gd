c3f18b0076c402c05aacf7232ad20572ac8c8448
Pau Capellá Ballester <pau.capella@hotmail.es>
2025-02-20 19:44:33 +0100

    ingredients generator working

--- potion-recall/Scripts/ingredient.gd
+++ potion-recall/Scripts/ingredient.gd
@@ -1,39 +1,19 @@
 extends RigidBody2D
 
-var mouseInIngredient = false
-var dragging = false
-var released = false
+var dragging = true
 
-func _ready():
+signal ingredientInPot
+
+func _ready() -> void:
 	gravity_scale = 0
-
-func _input(event):
-	if event is InputEventMouseButton:
-		
-		if event.is_pressed() && mouseInIngredient && !released:
-			
-			dragging = true
-			gravity_scale = 0
-			linear_velocity = Vector2.ZERO
-			set_freeze_mode(RigidBody2D.FREEZE_MODE_KINEMATIC)
-			
-		if event.is_released() && dragging:
-			
-			dragging = false
-			released = true
-			mouseInIngredient = false
-			gravity_scale = 1
-			position = global_position
+	linear_velocity = Vector2.ZERO
+	freeze = true
 
 func _physics_process(delta) -> void:
-	if dragging && !released:
-		position = get_global_mouse_position()
-
-func _on_mouse_entered() -> void:
-	mouseInIngredient = true
-
-func _on_mouse_exited() -> void:
-	mouseInIngredient = false
-
-func _on_drop_area_body_entered(body: Node2D) -> void:
-	body.queue_free()
+	if dragging:
+		self.position = get_global_mouse_position()
+		
+	if Input.is_action_just_released("left_click"):
+		dragging = false
+		gravity_scale = 1
+		freeze = false
