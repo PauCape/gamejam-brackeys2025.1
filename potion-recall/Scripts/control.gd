439d1f5a3b4558125c829ed53cdc7c95dfa9f427
marc-ms <myphysics28@gmail.com>
2025-02-20 12:05:51 +0100

    change level number label system added

--- potion-recall/Scripts/control.gd
+++ potion-recall/Scripts/control.gd
@@ -9,39 +9,48 @@ extends Control
 @onready var honeycomb: RigidBody2D = $"../Honeycomb"
 @onready var mushroom: RigidBody2D = $"../Mushroom"
 
+@onready var level_number_label: RichTextLabel = $LevelNumberLabel
+
 @export var levelNumber = 1
 @export var difficulty = 2
 var levelDifficulty = levelNumber * difficulty # == numero de items activos en lista
 
+var items: Array
+var isLevelCompleted: bool
+
 signal winningTime
 
 func _ready() -> void:
-	#ALERT: estaría bien var global para que no se inicialice el array en _ready
+	#ALERT: estaría bien  en otra funcion para que no se inicialice el array en _ready
 	var items = [berries, butterfly, egg, flower, garlic, herb, honeycomb, mushroom]
+	add_item_in_list(items)
+
+#func _process(delta: float) -> void:
+	#check_item_list_status()
+
+func add_item_in_list(items):
 	for i in pick_n_random_values(items, levelDifficulty):
 		$ItemList.add_item(i.name)
-
-func _process(delta: float) -> void:
-	check_item_list_status()
 
 func pick_n_random_values(array: Array, n: int) -> Array:
 	var array_copy: Array = array.duplicate()
 	array_copy.shuffle()
 	return array_copy.slice(0, n)
 
-func _on_button_pressed() -> void:
-	$ItemList.clear()
-	$LevelNumber.clear()
-	levelNumber = 1
-	$LevelNumber.add_text(str(levelNumber))
-	_ready()
-		
-
-func _on_increment_level_button_pressed() -> void:
-	$LevelNumber.clear()
-	_ready()
-	levelNumber += 1
-	$LevelNumber.add_text(str(levelNumber))
+#func _on_button_pressed() -> void:
+	#$ItemList.clear()
+	#$LevelNumber.clear()
+	#levelNumber = 1
+	#$LevelNumber.add_text(str(levelNumber))
+	#add_item_in_list(items)
+	
+func increment_level_label(completed) -> void:
+	if completed:
+		level_number_label.clear()
+		add_item_in_list(items)
+		levelNumber += 1
+		level_number_label.add_text(str(levelNumber))
+		_ready()
 
 # funcion que quitará el nombre del ingrediente metido en el POT
 func change_item_name(itemName: String) -> void:
@@ -52,38 +61,58 @@ func change_item_name(itemName: String) -> void:
 			pass
 
 # funcion que checkea si se ha resulto toda la lista
-func check_item_list_status() -> void:
+func check_item_list_status() -> bool:
 	var count = 0
+	isLevelCompleted = false
 	for i in range(0, levelDifficulty):
 		if $ItemList.get_item_text(i) == " ": # se podra añadir que el timer siga activo
 			count += 1
+			### si entra, nivel completado
 			if count == levelDifficulty:
-				print("You Win!")
-				emit_signal("winningTime")
+				isLevelCompleted = true
+			else:
+				pass
 		else: # se podra añadir si el timer acaba antes de resolver
 			pass
+	return isLevelCompleted
 
 #NOTE señales que emiten los ingredientes cuando entran el POT
 func _on_berries_ingredient_in_pot() -> void:
 	change_item_name("Berries")
+	check_item_list_status()
+	increment_level_label(isLevelCompleted)
 
 func _on_butterfly_ingredient_in_pot() -> void:
 	change_item_name("Butterfly")
+	check_item_list_status()
+	increment_level_label(isLevelCompleted)
 
 func _on_egg_ingredient_in_pot() -> void:
 	change_item_name("Egg")
+	check_item_list_status()
+	increment_level_label(isLevelCompleted)
 
 func _on_flower_ingredient_in_pot() -> void:
 	change_item_name("Flower")
+	check_item_list_status()
+	increment_level_label(isLevelCompleted)
 
 func _on_garlic_ingredient_in_pot() -> void:
 	change_item_name("Garlic")
+	check_item_list_status()
+	increment_level_label(isLevelCompleted)
 
 func _on_herb_ingredient_in_pot() -> void:
 	change_item_name("Herb")
+	check_item_list_status()
+	increment_level_label(isLevelCompleted)
 
 func _on_honeycomb_ingredient_in_pot() -> void:
 	change_item_name("Honeycomb")
+	check_item_list_status()
+	increment_level_label(isLevelCompleted)
 
 func _on_mushroom_ingredient_in_pot() -> void:
 	change_item_name("Mushroom")
+	check_item_list_status()
+	increment_level_label(isLevelCompleted)
--- potion-recall/Scripts/ingredient.gd
+++ potion-recall/Scripts/ingredient.gd
@@ -4,6 +4,8 @@ var mouseInIngredient = false
 var dragging = false
 var released = false
 
+@onready var berries: RigidBody2D = $"."
+
 signal ingredientInPot
 
 func _ready():
@@ -19,6 +21,7 @@ func _input(event):
 			linear_velocity = Vector2.ZERO
 			set_freeze_mode(RigidBody2D.FREEZE_MODE_KINEMATIC)
 			
+			
 		if event.is_released() && dragging:
 			
 			dragging = false
@@ -27,7 +30,7 @@ func _input(event):
 			gravity_scale = 1
 			position = global_position
 
-func _physics_process(delta) -> void:
+func _process(delta) -> void:
 	if dragging && !released:
 		position = get_global_mouse_position()
 
@@ -38,5 +41,10 @@ func _on_mouse_exited() -> void:
 	mouseInIngredient = false
 
 func _on_drop_area_body_entered(body: Node2D) -> void:
-	body.emit_signal("ingredientInPot")
-	body.queue_free()
+	if body.name == berries.name:
+		body.emit_signal("ingredientInPot")
+		body.queue_free()
+		print("Me repito")
+	#ALERT se repite tantas veces como numero de rigid bodies hay en la escena
+	# se debe a uqe estan conectados todos a la misma señal
+	
